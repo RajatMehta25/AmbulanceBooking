c516c848dce9457846228e2d72f1ca3560223f61
RajatMehta <rajat.mehta@codeaegis.in>
2024-07-31 16:16:15 +0530

    project

--- src/statics/constants.js
+++ src/statics/constants.js
@@ -1,7 +1,7 @@
 // export const API_URL = "http://15.184.202.217:3000";
 export const API_URL = "https://ltpatienttransportservices.co.uk/api";
-export const permsiions = ['user']
+export const permsiions = ["user"];
 // export const LOCATION_API_KEY="AIzaSyCPA6L1R1Q7GF7uHV4NMrvZwEEKDVibDkE";
 
 // export const LOCATION_API_KEY="AIzaSyAzWEYgzHRBWRwOJktxPWSJr4zTTZyxBdw";
- export const LOCATION_API_KEY = "AIzaSyDQxNESHnrKMHf-OPSlp05q9lSKLC2rhrQ";
+//  export const LOCATION_API_KEY = "AIzaSyDQxNESHnrKMHf-OPSlp05q9lSKLC2rhrQ";
